20c6e34237cc937f9abac3aeaa4d93e31df94d11
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-07-18 23:22:59 +0300

    basewidget: Add standard widget completion codes.

--- basewidget.py
+++ basewidget.py
@@ -3,6 +3,12 @@ import os
 from screen import *
 
 
+# Standard widget result actions (as return from .loop())
+ACTION_OK = 1000
+ACTION_CANCEL = 1001
+ACTION_NEXT = 1002
+ACTION_PREV = 1003
+
 class Widget(Screen):
 
     focusable = False
