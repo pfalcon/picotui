022018dc19bcad43a8bf3d5d34393922719e20c5
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-06-27 18:49:49 +0300

    setup.py: Add, for publishing to PyPI.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='picotui',
+      version='0.5',
+      description="""A simple text user interface (TUI) library.""",
+      url='https://github.com/pfalcon/picotui',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='MIT',
+      packages=['picotui'])
