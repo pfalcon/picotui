8b592ed2fc47faf8f056fcda0ca8dd88f75ec3f3
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-08-19 01:40:34 +0300

    setup.py: Release 0.7.

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 
 setup(name='picotui',
-      version='0.6',
+      version='0.7',
       description="""A simple text user interface (TUI) library.""",
+      long_description=open('README.rst').read(),
       url='https://github.com/pfalcon/picotui',
       author='Paul Sokolovsky',
       author_email='pfalcon@users.sourceforge.net',
