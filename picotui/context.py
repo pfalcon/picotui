c2ea47e7f793e4e2dab8b9e4dc149a3bd6385b5f
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-08-24 00:07:11 +0300

    context: Simple context manager to initialize picotui screen.

---
+++ picotui/context.py
@@ -0,0 +1,23 @@
+from .screen import Screen
+
+
+class Context:
+
+    def __init__(self, cls=True, mouse=True):
+        self.cls = cls
+        self.mouse = mouse
+
+    def __enter__(self):
+        Screen.init_tty()
+        if self.mouse:
+            Screen.enable_mouse()
+        if self.cls:
+            Screen.cls()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.mouse:
+            Screen.disable_mouse()
+        Screen.goto(0, 50)
+        Screen.cursor(True)
+        Screen.deinit_tty()
