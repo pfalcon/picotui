2dd8b5edfb6d91455032c9d78b1d6f57ae771778
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-04-30 21:24:20 +0300

    menu: ItemSelWidget: Rename move_focus() to move_sel().
    We use term "focus" to designate currently selected widget. So, avoid
    reusing it for internal widget items, instead consistently use term
    "selection".

--- picotui/menu.py
+++ picotui/menu.py
@@ -9,7 +9,7 @@ class ItemSelWidget(Widget):
         self.items = items
         self.selected = 0
 
-    def move_focus(self, direction):
+    def move_sel(self, direction):
         self.selected = (self.selected + direction) % len(self.items)
         self.redraw()
 
@@ -67,11 +67,11 @@ class WMenuBar(ItemSelWidget):
             elif key == KEY_LEFT:
                 if self.pulled_down:
                     self.screen_redraw()
-                self.move_focus(-1)
+                self.move_sel(-1)
             elif key == KEY_RIGHT:
                 if self.pulled_down:
                     self.screen_redraw()
-                self.move_focus(1)
+                self.move_sel(1)
             elif key == KEY_ENTER:
                 self.pulled_down = True
                 action = True
@@ -151,9 +151,9 @@ class WMenuBox(ItemSelWidget):
         if key == KEY_ESC:
             return ACTION_CANCEL
         elif key == KEY_UP:
-            self.move_focus(-1)
+            self.move_sel(-1)
         elif key == KEY_DOWN:
-            self.move_focus(1)
+            self.move_sel(1)
         elif key == KEY_LEFT:
             return ACTION_PREV
         elif key == KEY_RIGHT:
