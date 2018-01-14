5b7b72f61f5cba56923177342620890da44cb000
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-01-15 00:14:14 +0200

    editor: Explicitly call Widget constructor.
    To not play tricks will multiple inheritance diamond patterns.

--- picotui/editor.py
+++ picotui/editor.py
@@ -13,7 +13,7 @@ from .basewidget import Widget
 class Editor(Widget):
 
     def __init__(self, x=0, y=0, width=80, height=24):
-        super().__init__()
+        Widget.__init__(self)
         self.top_line = 0
         self.cur_line = 0
         self.row = 0
